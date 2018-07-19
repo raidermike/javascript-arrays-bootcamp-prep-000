3d6dc079d6caf55a5abee19fe2344707ae331707
mjcabrera@gmail.com <mjcabrera@gmail.com>
2018-07-19 18:41:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,10 +5,8 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function descructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
-
-  return array
+function descructivelyAddElementToBeginningOfArray(){
+  
 }
 
 function addElementToEndOfArray(array, element) {
